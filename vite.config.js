00cfebd9c6c38376c3cb39e6fd5d2acf9562f9f0
jafferafzalkhan <jaffarkhan2495@gmail.com>
2026-03-30 16:15:52 +0530

    fixed

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+``
+// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api/coingecko': {
-        target: 'https://api.coingecko.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/coingecko/, ''), // Removes '/api/coingecko' prefix
-      },
-    },
-  },
-});
+  plugins: [react(),
+    tailwindcss(),
+  ],
+})
