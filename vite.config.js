48b4a719148a5a7c163665d0b71eb4591125f1ed
jafferafzalkhan <jaffarkhan2495@gmail.com>
2026-03-30 14:50:57 +0530

    fixed reload problem

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-``
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
-})
+  plugins: [react(), tailwindcss()],
+
+  build: {
+    chunkSizeWarningLimit: 1000, // ✅ optional (reduce warning noise)
+
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          router: ["react-router-dom"],
+        },
+      },
+    },
+  },
+});
